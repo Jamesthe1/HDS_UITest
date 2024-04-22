85a6daf999ce1890cbf0b12492da370662635648
Jamesthe1 <jamesthe1sky@gmail.com>
2024-04-22 10:29:34 -0500

    Make plugin dependent on Nebula

--- Plugin.cs
+++ Plugin.cs
@@ -4,44 +4,42 @@ using BepInEx;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+using Nebula;
+using Nebula.UI;
+using Nebula.Utils;
+
 namespace UITest {
     [BepInPlugin (PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    [BepInDependency ("Nebula")]
     [BepInProcess ("dyingsun.exe")]
     public class Plugin : BaseUnityPlugin {
         private void Awake () {
             // Plugin startup logic
-            Logger.LogInfo ($"Plugin {PluginInfo.PLUGIN_GUID} welcomes you with a lot of logging");
-            SceneManager.sceneLoaded += SceneLoadHook;
+            NebulaPlugin.SceneReady += LoadUI;
         }
 
-        private void SceneLoadHook (Scene scene, LoadSceneMode mode) {
-            SceneManager.sceneLoaded -= SceneLoadHook;
-            Logger.LogInfo ($"Caught scene loading: {scene.name}; starting coroutine");
-            StartCoroutine (LoadUI (scene));
-        }
+        private void LoadUI (Scene scene) {
+            NebulaPlugin.SceneReady -= LoadUI;
 
-        private IEnumerator LoadUI (Scene scene) {
-            yield return null;          // Wait one frame for the scene to fully load
             Logger.LogInfo ($"Loading UI elements...");
             
-            GameObject goRoot = null;   // Using a master variable instead of multiple variables that get discarded afterward
-            UIButton button = null;
             Vector3 buttonSize = new Vector3 (352, 24, 0);
+            int fontSize = 20;
 
-            GameObject uiRoot = scene.GetRootGameObjects ().GetGameObject ("# CUI_2D");
-            Transform menuRoot = uiRoot.transform.FindChild ("Camera/ROOT_Menus");
-
-            Transform mainRoot = menuRoot.FindChild ("GATE_MainMenu/ANCHOR_MainMenu/PANEL_MainMenu/ROOT_MainMenu");
-            UILabel motd = mainRoot.FindChild ("WIDGET_MOTD/PANEL_ScrollWindow/010_LABEL_BlockText").GetComponent<UILabel> ();
-            goRoot = mainRoot.FindChild ("ROOT_Left/010_BUTTONS").gameObject;
+            string menuRoot = "GATE_MainMenu/ANCHOR_MainMenu/PANEL_MainMenu/ROOT_MainMenu";
+            string buttonsPath = "ROOT_Left/010_BUTTONS";
 
             List<EventDelegate> delegates = new List<EventDelegate> ();
             EventDelegate.Add (delegates, HelloWorld);
-            
-            button = goRoot.CreateButton (buttonSize, "HelloWorld", "HELLO WORLD", delegates, 20, Color.cyan);
-            button.name = $"021_{button.name}"; // Places the button below "PLAY"
 
-            mainRoot.GetComponent<CUIMainMenu> ().AddTooltip (motd, button.gameObject, "Prints \"Hello world!\" to the console.");
+            ButtonDetailDatum detail = new ButtonDetailDatum (Color.cyan);
+            ButtonFactoryDatum factoryDatum = new ButtonFactoryDatum ("HelloWorld", "HELLO WORLD", delegates, fontSize, buttonSize, menuRoot, buttonsPath, detail);
+
+            UIButton button = ButtonFactory.Create (factoryDatum);
+            MOTDDatum motdDatum = new MOTDDatum ("Prints \"Hello world!\" to the console.");
+            ButtonDatum buttonDatum = new ButtonDatum (button, menuRoot, 21, motdDatum);
+
+            ButtonSpawner.buttonQueue.Enqueue (buttonDatum);
         }
 
         private void HelloWorld () {
